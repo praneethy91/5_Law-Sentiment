17e025585de2792221b4c503b88a2d36ff04ca15
namanksrivastava <namanksrivastava@gmail.com>
2017-04-20 21:58:01 -0400

    Changed Path : Naman

--- ProcessJudgeData.py
+++ ProcessJudgeData.py
@@ -8,41 +8,42 @@ username = 'nk2239'
 outDir = '/home/' + username + '/Aggregate/'
 
 def get_case_level_data_frame():
-	data_dir = 'data'
-	if demo_local:
-		data_dir = 'Bloomberg'
-	case_level_dta_file_name = 'BloombergCASELEVEL_Touse.dta'
-	case_level_dta_abs_path = os.path.abspath(data_dir + '/' + case_level_dta_file_name)
-	case_data_df = pd.read_stata(case_level_dta_abs_path)
-	sliced_data=case_data_df[['caseid','Author']]
-	return sliced_data[sliced_data['Author']!='']
+    data_dir = 'data'
+    if demo_local:
+        data_dir = 'Bloomberg'
+    case_level_dta_file_name = 'BloombergCASELEVEL_Touse.dta'
+    case_level_dta_abs_path = os.path.abspath(data_dir + '/' + case_level_dta_file_name)
+    case_data_df = pd.read_stata(case_level_dta_abs_path)
+    sliced_data=case_data_df[['caseid','Author']]
+    return sliced_data[sliced_data['Author']!='']
 
 
 def create_dict_of_judges_cases(data_frame):
-	judges_dict={}
-	#cases_data=get_case_level_data_frame()
-	cases_data = data_frame
-	for index,row in cases_data.iterrows():
-	  if not row['Author'] in judges_dict:
-	    judges_dict[row['Author']]=[]
-	  judges_dict[row['Author']].append(row['caseid'])
-	return judges_dict 
+    judges_dict={}
+    #cases_data=get_case_level_data_frame()
+    cases_data = data_frame
+    for index,row in cases_data.iterrows():
+      if not row['Author'] in judges_dict:
+        judges_dict[row['Author']]=[]
+      judges_dict[row['Author']].append(row['caseid'])
+    return judges_dict
 
 
 def get_relative_path_of_cases():
-	root_dir = 'data/clean_Mar_20'
-	if demo_local:
-		root_dir = '../../Data/clean_Mar_20'
-	years=utl.getDirectoryList(root_dir)
-	if demo_local:
-		years = ['1964']
-	paths={}
-	for year in years:
-		if not year.endswith('zip') and year >= '1964':
-			cases_files=utl.getFilesListFromDir(year)
-			for case in cases_files:
-				key = case.replace('-maj.p','')
-				paths[key]=outDir + '/CaseLevel/'+ year+'/'+ case
-	return paths
+    root_dir = 'data/clean_Mar_20'
+    if demo_local:
+        root_dir = '../../Data/clean_Mar_20'
+    years=utl.getDirectoryList(root_dir)
+    if demo_local:
+        outDir = 'Aggregate'
+        years = ['1964']
+    paths={}
+    for year in years:
+        if not year.endswith('zip') and year >= '1964':
+            cases_files=utl.getFilesListFromDir(year)
+            for case in cases_files:
+                key = case.replace('-maj.p','')
+                paths[key]=outDir + '/CaseLevel/'+ year+'/'+ case
+    return paths
 
 
--- aggregate_results.py
+++ aggregate_results.py
@@ -2,25 +2,35 @@ import ProcessJudgeData as pjd
 import utils as util
 import numpy as np
 import pickle as pkl
+
 username = 'nk2239'
 outDir = '/home/' + username + '/Aggregate/'
+demo_local = False
 
 def case_level():
     sentiment_dir = '/home/' + username + '/VADER_DATA_STORE/'
     similarity_dir = '/home/' + username + '/SIMILARITY_DATA_STORE/'
+    if(demo_local):
+        sentiment_dir = 'VADER_DATA_STORE/'
+        similarity_dir = 'SIMILARITY_DATA_STORE/'
+        outDir = 'Aggregate'
     list_similarity_dir = util.getDirectoryList(similarity_dir)
     outDirectory = outDir + '/CaseLevel'
     for directory in list_similarity_dir:
-        util.createDirectory(outDirectory)
-        util.createDirectory(outDirectory + "/" + directory)
-        files = util.getFilesListFromDir(directory)
-        for file in files:
-            sentiment_list = util.getDataFromPickle(file, sentiment_dir + directory + '/')
-            similarity_list = util.getDataFromPickle(file, similarity_dir + directory + '/')
-            if(len(similarity_list) == len(sentiment_list)):
-                util.writeToPickle(np.dot(sentiment_list,similarity_list),outDir,directory,file)
+        if not directory.endswith('zip'):
+            util.createDirectory(outDirectory)
+            util.createDirectory(outDirectory + "/" + directory)
+            files = util.getFilesListFromDir(directory)
+            for file in files:
+                sentiment_list = util.getDataFromPickle(file, sentiment_dir + directory + '/')
+                similarity_list = util.getDataFromPickle(file, similarity_dir + directory + '/')
+                if(len(similarity_list) == len(sentiment_list)):
+                    util.writeToPickle(np.dot(sentiment_list,similarity_list),outDir,directory,file)
 
-def judge_level(judge_to_case_dict, case_to_path_dict):
+def judge_level():
+    data_frame = pjd.get_case_level_data_frame()
+    judge_to_case_dict = pjd.create_dict_of_judges_cases(data_frame)
+    case_to_path_dict = pjd.get_relative_path_of_cases()
     judges_to_score={}
     for judge, case_list in judge_to_case_dict.items():
         current_judge_score=None
@@ -32,15 +42,11 @@ def judge_level(judge_to_case_dict, case_to_path_dict):
             else :
                 current_judge_score+=current_score
         judges_to_score[judge]=current_judge_score/len(case_list)
-
     return judges_to_score
 
 def main():
-    data_frame = pjd.get_case_level_data_frame()
-    judge_to_case = pjd.create_dict_of_judges_cases(data_frame)
-    case_to_path = pjd.get_relative_path_of_cases()
     case_level()
-    judge_level(judge_to_case, case_to_path)
+    #judge_level()
 
 
 
