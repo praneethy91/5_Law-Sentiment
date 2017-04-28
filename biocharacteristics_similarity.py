0ec2d5cc5bf14bf1a2efacf7a8dca089fa9056af
praneethy91 <praneethy91@gmail.com>
2017-04-28 18:56:36 -0400

    Adding code for computing unweighted and weighted average judge bio characteristics

--- biocharacteristics_similarity.py
+++ biocharacteristics_similarity.py
@@ -1,19 +1,98 @@
 import os
+from contextlib import ExitStack
+
 import pandas as pd
+import configparser
+import pickle as pck
+import numpy as np
+import codecs
+import sys
 
-data_dir = 'data'
-case_level_dta_file_name = 'BloombergCASELEVEL_Touse.dta'
-case_level_dta_abs_path = os.path.abspath(os.path.join(data_dir, case_level_dta_file_name))
-case_data_df = pd.read_stata(case_level_dta_abs_path)
+# Reading values of the configuration file
+config = configparser.ConfigParser()
+config.read('config.ini')
+case_similarities_dir = config['directories']['case_level_similarities']
+vote_level_dataset = config['directories']['vote_level_dataset']
+circuit_year_to_case_file = config['directories']['circuit_year_to_case_dataset']
+bio_weighted_avg_file = config['directories']['biocharacteristics_weighted_avg']
+bio_unweighted_avg_file = config['directories']['biocharacteristics_unweighted_avg']
 
-judgeBioCharacteristics = ['party', 'Senior', 'gender', 'score']
-judgePrefixes = ['j1', 'j2', 'j3']
-allJudgesColumns = [(judgePrefix + characteristic)
-                    for judgePrefix in judgePrefixes
-                        for characteristic in judgeBioCharacteristics]
+case_data_df_iterator = pd.read_stata(vote_level_dataset, iterator=True, chunksize=1000)
+bio_columns = ['x_dem', 'x_republican', 'x_instate_ba', 'x_elev', 'x_unity',
+               'x_aba', 'x_crossa', 'x_pfedjdge', 'x_pindreg1', 'x_plawprof',
+               'x_pscab', 'x_pcab', 'x_pusa', 'x_pssenate', 'x_paag', 'x_psp',
+               'x_pslc', 'x_pssc', 'x_pshouse', 'x_psg', 'x_psgo', 'x_psenate',
+               'x_psatty', 'x_pprivate', 'x_pmayor', 'x_plocct', 'x_phouse',
+               'x_pgov', 'x_pda', 'x_pcc', 'x_pccoun', 'x_pausa', 'x_pasatty',
+               'x_pag', 'x_pada', 'x_pgovt', 'x_llm_sjd', 'x_protestant',
+               'x_evangelical', 'x_mainline', 'x_noreligion', 'x_catholic',
+               'x_jewish', 'x_black', 'x_nonwhite', 'x_female', 'x_jd_public',
+               'x_ba_public', 'x_b10s', 'x_b20s', 'x_b30s', 'x_b40s', 'x_b50s',
+               'x_pbank', 'x_pmag', 'x_ageon40s', 'x_ageon50s', 'x_ageon60s',
+               'x_ageon40orless', 'x_ageon70ormore', 'x_pago']
 
-# Code for exploring the columns of the judge bio-characteristic data
-# for column in allJudgesColumns:
-#     print('------------------{0}-------------------'.format(column))
-#     print(case_data_df[column].unique())
+bio_weighted_dict = {}
+bio_unweighted_dict = {}
 
+bio_weighted_num_dict = {}
+bio_unweighted_num_dict = {}
+
+# case_data_df_iterator_small = pd.read_stata(vote_level_dataset, iterator=True, chunksize=10)
+# case_data_df_chunk = case_data_df_iterator_small.__next__()
+# col_id_map = {}
+# col_id_map['Circuit'] = case_data_df_chunk.columns.get_loc('Circuit')
+# col_id_map['year'] = case_data_df_chunk.columns.get_loc('year')
+# for bio_column in bio_columns:
+#     col_id_map[bio_column] = case_data_df_chunk.columns.get_loc(bio_column)
+
+with open(circuit_year_to_case_file, 'rb') as f:
+    ckt_case_map  = pck.load(f)
+
+for (ckt, year), case_df in ckt_case_map.items():
+    ckt = int(ckt)
+    year = int(year)
+    if year >= 1964:
+        bio_weighted_dict[(ckt, year)] = {}
+        bio_unweighted_dict[(ckt, year)] = {}
+        bio_weighted_num_dict[(ckt, year)] = {}
+        bio_unweighted_num_dict[(ckt, year)] = {}
+        for bio_column in bio_columns:
+            bio_weighted_dict[(ckt, year)][bio_column] = np.zeros(40)
+            bio_unweighted_dict[(ckt, year)][bio_column] = np.zeros(40)
+            bio_unweighted_num_dict[(ckt, year)][bio_column] = np.zeros(40)
+            bio_weighted_num_dict[(ckt, year)][bio_column] = np.zeros(40)
+
+chunk = 1
+for case_data_df_chunk in case_data_df_iterator:
+    for index, row in case_data_df_chunk.iterrows():
+        ckt = row['Circuit']
+        year = row['year']
+        if(~ np.isfinite(ckt) or ~ np.isfinite(year)):
+            continue
+        year = int(row['year'])
+        case = row['caseid']
+        case_path = os.path.join(case_similarities_dir, str(year), (case.upper() + '-maj.p'))
+        if os.path.isfile(case_path) and year >= 1964:
+            with open(case_path, mode='rb') as f:
+                case_similarity = pck.load(f)
+            for column in bio_columns:
+                value = row[column]
+                if(~ np.isfinite(value)):
+                    continue
+                bio_weighted_dict[(ckt, year)][column] += value * case_similarity
+                bio_unweighted_dict[(ckt, year)][column] += value * np.ones(40)
+                bio_weighted_num_dict[(ckt, year)][column] += case_similarity
+                bio_unweighted_num_dict[(ckt, year)][column] += np.ones(40)
+    print('Chunk: ' + str(chunk))
+    chunk += 1
+
+for (ckt, year), ls in ckt_case_map.items():
+    ckt = int(ckt)
+    year = int(year)
+    if year >= 1964:
+        for bio_column in bio_columns:
+            bio_weighted_dict[(ckt, year)][bio_column] /= bio_weighted_num_dict[(ckt, year)][bio_column]
+            bio_unweighted_dict[(ckt, year)][bio_column] /= bio_unweighted_num_dict[(ckt, year)][bio_column]
+
+pck.dump(bio_weighted_dict, open('data\\bio_weighted_dict.pkl', 'wb'))
+pck.dump(bio_unweighted_dict, open('data\\bio_unweighted_dict.pkl', 'wb'))
