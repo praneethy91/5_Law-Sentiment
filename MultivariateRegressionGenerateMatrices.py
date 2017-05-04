3e1269a3e553bfd48fe91652a5e7637a233158b9
praneethy91 <praneethy91@gmail.com>
2017-05-04 19:15:50 -0400

    Resolving #16 #18 #22.

--- MultivariateRegressionGenerateMatrices.py
+++ MultivariateRegressionGenerateMatrices.py
@@ -2,18 +2,40 @@ import os
 import pickle as pkl
 import numpy as np
 import pandas as pd
+import functools
 
-home_folder="/home/bsg348/"
+home_folder="data/"
 bio_unweighted='bio_unweighted_dict.pkl'
 bio_weighted='bio_weighted_dict.pkl'
 bio_folder="bioaverage/"
 similarity_folder="CircuitYearLevel/"
+anes_folder = "outcome_var/"
 
+def cmp_items(a, b):
+    m = a.split("_")
+    circuit_a = int(m[0])
+    year_a = int(m[1])
 
-circuit_year=os.listdir(home_folder+bio_folder)
+    s = b.split("_")
+    circuit_b = int(s[0])
+    year_b = int(s[1])
+
+    if circuit_a > circuit_b:
+        return 1
+    elif circuit_a == circuit_b:
+        if year_a > year_b:
+            return 1
+        else:
+            return -1
+    else:
+        return -1
+
+circuit_year = os.listdir(home_folder+anes_folder)
+circuit_year = sorted(circuit_year, key=functools.cmp_to_key(cmp_items))
+
 #this part is hardcoded as we know the data
 #and are optimizing time complexity using vectorization
-number_of_ckt_years=616
+number_of_ckt_years=248 # got from ANES data
 number_of_bio_characteristics=61
 number_of_thermometers=40
 
@@ -21,27 +43,22 @@ number_of_thermometers=40
 
 #Z_matrix represents weighted average biocharacteristic
 Z_matrix=np.zeros((number_of_ckt_years*number_of_thermometers,number_of_bio_characteristics))
-#X_matrix represents unweighted average biocharacteristic
-X_matrix=np.zeros((number_of_ckt_years*number_of_thermometers,number_of_bio_characteristics))
+
 #S_matrix represents the similarity
 S_matrix=np.zeros((number_of_ckt_years*number_of_thermometers,1))
 counter=0
 
-
-df = pd.DataFrame(columns=('circuit_thermometer','thermometer_year'))
+df = pd.DataFrame(columns=('circuit_thermometer','thermometer_year', 'circuit_year'))
 
 dataframe_index=0
 for ckt_yr in circuit_year:
     print(ckt_yr)
     name_file_similarity=home_folder+similarity_folder+ckt_yr+".0.p"
     name_file_bio_weighted=home_folder+bio_folder+ckt_yr+"/"+bio_weighted
-    name_file_bio_unweighted=home_folder+bio_folder+ckt_yr+"/"+bio_unweighted
     file_similarity=pkl.load(open(name_file_similarity,'rb'))
 
     file_bio_weighted=pkl.load(open(name_file_bio_weighted,'rb'))
     #print("abc",file_bio_weighted['x_phouse'])
-    file_bio_unweighted = pkl.load(open(name_file_bio_unweighted, 'rb'))
-    #print("pqr",file_bio_unweighted['x_phouse'])
     S_matrix[counter:counter+number_of_thermometers]=file_similarity.reshape((number_of_thermometers,1))
 
     incr=0
@@ -51,35 +68,36 @@ for ckt_yr in circuit_year:
     for bio_char in file_bio_weighted.keys():
         #print(bio_char)
         Z_matrix[counter:counter+number_of_thermometers,incr]=file_bio_weighted[bio_char]
-        #print("abc",file_bio_weighted[bio_char])
-        #print("pqr",file_bio_unweighted[bio_char])
-        X_matrix[counter:counter+number_of_thermometers,incr]=file_bio_unweighted[bio_char]
         incr += 1
-
 
     counter+=number_of_thermometers
 
     for therm in range(number_of_thermometers):
-        print("ck",circuit_number +"_"+ str(therm))
-        print("kt",str(therm) + "_"+year_number)
-        df.loc[dataframe_index] = [circuit_number +"_"+ str(therm), str(therm)  +"_"+year_number]
+        # print("ck",circuit_number +"_"+ str(therm))
+        # print("kt",str(therm) + "_"+year_number)
+        df.loc[dataframe_index] = [circuit_number +"_"+ str(therm), str(therm)  +"_"+year_number, circuit_number +"_"+ year_number]
         dataframe_index += 1
 
 print("dumping Z matrix")
 pkl.dump(Z_matrix,open(home_folder+"Z_matrix.pkl","wb"))
-print("dumping X matrix")
-pkl.dump(X_matrix,open(home_folder+"X_matrix.pkl","wb"))
 print("dumping S matrix")
 pkl.dump(S_matrix,open(home_folder+"S_matrix.pkl","wb"))
-print(df.iloc[100:200])
+
 dummies_ck=pd.get_dummies(df[['circuit_thermometer']])
 dummies_kt=pd.get_dummies(df[['thermometer_year']])
-print(dummies_ck.shape)
+dummies_ct=pd.get_dummies(df[['circuit_year']])
+
 print(Z_matrix.shape)
+print(dummies_ck.shape)
 print(dummies_kt.shape)
+print(dummies_ct.shape)
+
 #print(dummies_ck.iloc[0])
 #print(dummies_kt.iloc[0])
+
 print("dumping gamma_ck")
 pkl.dump(dummies_ck,open(home_folder+"gamma_ck","wb"))
 print("dumping gamma_kt")
 pkl.dump(dummies_ck,open(home_folder+"gamma_kt","wb"))
+print("dumping gamma_ct")
+pkl.dump(dummies_ck,open(home_folder+"gamma_ct","wb"))
