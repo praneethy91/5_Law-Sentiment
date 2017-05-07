cda563b3062f32609af9f046f556fe08382e9909
rohitmuthyala <rrm404@nyu.edu>
2017-05-07 15:09:45 -0400

    Add files via upload

--- Top10_words_cosinesim.py
+++ Top10_words_cosinesim.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-import utils
 import numpy as np
 from gensim.models.word2vec import Word2Vec
 
@@ -15,8 +14,9 @@ thermometers = ['democrats', 'republicans', 'protestants', 'catholics', 'jews', 
                 'christian fundamentalists', 'radical students', 'farmers', 'feminists', 'evangelical groups',
                 'elderly', 'supreme court', 'women']
 word2Vec_dimension = 100
-data_dir = 'data'
-case_dir = os.path.join(data_dir, 'clean_Mar_20')
+#data_dir = 'data'
+#case_dir = os.path.join(data_dir, 'clean_Mar_20')
+case_dir = '/scratch/bsg348-share/MLCS/data/clean_Mar_20'
 maj_dir = 'maj'
 thermometer_vectors = np.zeros((len(thermometers), word2Vec_dimension))
 i = 0
@@ -29,11 +29,15 @@ for thermometer in thermometers:
     i += 1
 Top10words = [[0 for x in range(10)] for y in range(40)]
 Top10Similarities = [[0 for x in range(10)] for y in range(40)]
-
+num = [0 for x in range(65)];
 norm_thermometer = np.linalg.norm(thermometer_vectors, axis=1)
 normalized_thermometer_vectors = (np.true_divide(thermometer_vectors, norm_thermometer[:, None]))
 list_of_dirs = [d for d in os.listdir(case_dir) if os.path.isdir(os.path.join(case_dir, d))]
+i = 1;
 for directory in list_of_dirs:
+    num[0] = num[0] +1;
+    num[i] = directory;
+    i = i +1;
     print(directory)
     files = os.listdir(os.path.join(case_dir, directory, maj_dir))
     for file_name in files:
@@ -63,4 +67,28 @@ for directory in list_of_dirs:
                 except:
                     pass
     print(Top10Similarities);
+    with open('outfile_w_2', 'wb') as fp:
+        pickle.dump(Top10words, fp)
+    with open('outfile_s_2', 'wb') as fp:
+        pickle.dump(Top10Similarities, fp)
     print(Top10words);
+    with open('outfile_num', 'wb') as fp:
+        pickle.dump(num, fp)
+    '''
+    with open ('outfile', 'rb') as fp:
+        itemlist = pickle.load(fp)
+    '''
+i = 0;
+temp = []
+for X in Top10words:
+  Y = Top10Similarities[i]
+  temp.append([x for (y,x) in sorted(zip(Y,X))])
+  i = i + 1;
+result = []
+for X in temp:
+    T = [i for i in reversed(X)]
+    result.append(T)
+print(result)
+sim = [];
+for X in Top10Similarities:
+    sim.append(sorted(X,reverse=True))
